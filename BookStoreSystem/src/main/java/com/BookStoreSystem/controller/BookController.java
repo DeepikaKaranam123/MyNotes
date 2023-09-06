72ba4a9f1d2ce775027507da4f941706a5eb6f9f
DeepikaKaranam123 <138875921+DeepikaKaranam123@users.noreply.github.com>
2023-09-06 09:00:41 +0530

    Add files via upload

---
+++ BookStoreSystem/src/main/java/com/BookStoreSystem/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.BookStoreSystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+@Controller
+
+public class BookController {
+	@GetMapping("/")
+	public String home()
+	{
+		return "home";
+	}
+	@GetMapping("/book_register")
+	public String bookRegister()
+	{
+		return "bookRegister";
+	}
+}
---
+++ BookStoreSystem/src/test/java/com/BookStoreSystem/BookStoreSystem/BookStoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookStoreSystem.BookStoreSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
